59e11ceeba1dd64295f9588a7e1f5c8906e367ec
Maxen <maxen@composolution.com>
2024-10-22 12:17:01 +0530

    roman to integer

--- src/kata.controller.spec.ts
+++ src/kata.controller.spec.ts
@@ -12,4 +12,25 @@ describe('AppController', () => {
 
     appController = app.get<AppController>(AppController);
   });
+
+  describe('Convert Roman to Integer', () => {
+    it('Roman numerals 1', () => {
+      expect(appController.romanToInteger('XLI')).toBe(41);
+    });
+    it('Roman numerals 2', () => {
+      expect(appController.romanToInteger('CCXXXVI')).toBe(236);
+    });
+    it('Roman numerals 3', () => {
+      expect(appController.romanToInteger('DCCXLV')).toBe(745);
+    });
+    it('Roman numerals 4', () => {
+      expect(appController.romanToInteger('LXVI')).toBe(66);
+    });
+    it('Roman numerals 5', () => {
+      expect(appController.romanToInteger('CDXLVII')).toBe(447);
+    });
+    it('Roman numerals 6', () => {
+      expect(appController.romanToInteger('LXXXIX')).toBe(89);
+    });
+  });
 });
--- src/kata.controller.ts
+++ src/kata.controller.ts
@@ -4,7 +4,17 @@ import { Controller, Get } from '@nestjs/common';
 export class AppController {
   constructor() {}
 
-  add(input: string): number {
+  romanToInteger(romanNumber: string): number {
+    const romanValues = {
+      I: 1,
+      V: 5,
+      X: 10,
+      L: 50,
+      C: 100,
+      D: 500,
+      M: 1000,
+    };
+
     return 0;
   }
 }
